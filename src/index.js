f4f4895bfa795d7265bf24811b8ebe52a3f898f4
bpalowski <brianpalowski00@gmail.com>
2019-06-15 22:15:15 -0700

    End of class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Season from './Season'
 
-const App = () => {
-	return (
 
-		<div className="ui container comments">
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			lat: null,
+			errorMsg: ''
+		};
+		window.navigator.geolocation.getCurrentPosition(
+			pos => {
+				this.setState({
+					lat: pos.coords.latitude
+				})
+			},
+			err => {
+				this.setState({
+					errorMsg: err.message
+				})
+			}
+		);
+	}
+	render() {
+		if (this.state.errorMsg && !this.state.lat) {
+			return <div> Err: {this.state.errorMsg}</div>
+		}
+		if (!this.state.errorMsg && this.state.lat) {
+			return <div>
+				<h1>Lattitude: {this.state.lat}  </h1>
+			</div>
+		}
+		return <div>Loading !</div>
+	}
 
-		</div>
-
-	)
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
