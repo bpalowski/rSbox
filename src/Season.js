6e3f27ea74dba30566cf643bb4bfdc75c4282636
bpalowski <brianpalowski00@gmail.com>
2019-06-15 21:34:06 -0700

    New Component with location

---
+++ src/Season.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Season = () => {
+
+	return (
+		<div>
+
+		</div>
+	)
+}
+
+
+export default Season
--- src/index.js
+++ src/index.js
@@ -1,37 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import faker from 'faker';
-import ApprovalCArd from './ApprovalCard';
+import Season from './Season'
 
-import Comment from './Comment'
 const App = () => {
 	return (
 
 		<div className="ui container comments">
-			<br />
-			<ApprovalCArd >
-				<div>
-					<h4>Warning</h4>
-				</div>
-			</ApprovalCArd>
 
-
-
-			<ApprovalCArd >
-				<Comment
-					author={faker.name.firstName()} content="Nice Job" />
-			</ApprovalCArd>
-
-			<ApprovalCArd >
-				<Comment
-					author={faker.name.firstName()} content="Me like" />
-			</ApprovalCArd>
-
-			<ApprovalCArd >
-				<Comment
-					author={faker.name.firstName()} content="Yes" />
-			</ApprovalCArd>
 		</div>
 
 	)
