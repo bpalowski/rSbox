ecfd535906fd6f53d66bd3d2b9acecc3e40277bc
bpalowski <brianpalowski00@gmail.com>
2019-06-23 14:58:23 -0700

    Updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 
-const App = () => {
-	return (
+import App from './App'
 
-		<div>Hello</div>
-
-	)
-}
 
 ReactDOM.render(<App />, document.getElementById('root'));
