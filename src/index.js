7728a4cde044780adf952f55b6c2ae5991368a06
bpalowski <brianpalowski00@gmail.com>
2019-06-23 14:20:22 -0700

    Remoevd files

--- src/index.js
+++ src/index.js
@@ -1,38 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import faker from 'faker';
-import ApprovalCArd from './ApprovalCard';
 
-import Comment from './Comment'
 const App = () => {
 	return (
 
-		<div className="ui container comments">
-			<br />
-			<ApprovalCArd >
-				<div>
-					<h4>Warning</h4>
-				</div>
-			</ApprovalCArd>
-
-
-
-			<ApprovalCArd >
-				<Comment
-					author={faker.name.firstName()} content="Nice Job" />
-			</ApprovalCArd>
-
-			<ApprovalCArd >
-				<Comment
-					author={faker.name.firstName()} content="Me like" />
-			</ApprovalCArd>
-
-			<ApprovalCArd >
-				<Comment
-					author={faker.name.firstName()} content="Yes" />
-			</ApprovalCArd>
-		</div>
+		<div>Hello</div>
 
 	)
 }
