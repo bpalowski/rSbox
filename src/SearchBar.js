319ac4a35aca92a97fbd07f3ea2d6d851b9b9dbc
bpalowski <brianpalowski00@gmail.com>
2019-06-23 15:46:17 -0700

    Updated with onClick

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,19 +1,29 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+
+	onInputChange(e) {
+		console.log(e.target.value);
+	}
+	onInputClick() {
+		console.log("there has been a change")
+	}
+
 	render() {
+
 		return (
 			<div className="ui segment">
 				<form className="ui form">
 					<div className="field">
 						<label htmlFor="">Image Search</label>
-						<input type="text" />
+						<input type="text"
+							onClick={this.onInputClick} onChange={this.onInputChange} />
 					</div>
 				</form>
 			</div>
 		);
-	}
 
+	}
 }
 
 
